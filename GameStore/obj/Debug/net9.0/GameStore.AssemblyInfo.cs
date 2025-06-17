f31985bf1a19a89419d5aa26af2fe5c6f0176fa6
Abdul Rahim <k213951@nu.edu.pk>
2025-06-17 13:18:35 +0500

    updated gitinore

--- GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d225d3c876c04a3da85eeae0bac910b005304e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
